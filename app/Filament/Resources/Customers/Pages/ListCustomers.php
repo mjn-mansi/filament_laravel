74eb32db847321c25be0a940030010cd7c6396c7
Maansi Mahajan <mahajan.maansi13@gmail.com>
2026-03-09 16:28:29 +0530

    fix: added Tabs in Customer

--- app/Filament/Resources/Customers/Pages/ListCustomers.php
+++ app/Filament/Resources/Customers/Pages/ListCustomers.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Customers\Pages;
 
 use App\Filament\Resources\Customers\CustomerResource;
+use App\Models\Customer;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCustomers extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListCustomers extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'active' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_active', true))
+                ->badge(Customer::query()->where('is_active', true)->count())->badgeColor('success'),
+            'inactive' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_active', false))
+                ->badge(Customer::query()->where('is_active', false)->count()),
         ];
     }
 }
