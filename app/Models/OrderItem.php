b3f66c3a985dcf5175393e07438a5a52edb431d1
Maansi Mahajan <mahajan.maansi13@gmail.com>
2026-03-09 18:07:10 +0530

    Order model & migrations

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = ['order_id', 'item_id', 'quantity', 'price', 'total'];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function item() {
+        return $this->belongsTo(Item::class);
+    }
+}
